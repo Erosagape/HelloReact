d370e7e4a79da71e5555a7416065a652264cf90b
jobtawan <leoputti@hotmail.com>
2019-02-27 13:39:24 +0700

    1st Commit

---
+++ my-app/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div class="App">
+                <div class="App-header">
+                    Hello, There!
+                </div>
+                <div class="App-content">                
+                    My First React Application
+                </div>    
+                <div class="App-footer">
+                    This is footer
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ readme.md
@@ -0,0 +1,16 @@
+****React
+
+Setting Required:
+  node.js v8
+  jdk 1.8
+  
+npm setup:
+  npm install -g create-react-app
+
+create new project:
+  create-react-app <ชื่อโปรเจค>
+  
+running server:
+  cd <ชื่อโปรเจค>
+  npm start
+  
